ed6cf98270d470507b65717bffce2d91359145ca
Jonathanrange <jonathan.range@outlook.com>
2022-01-13 15:38:19 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ snareOne.addEventListener('click', popUp);
 function popUp(){
     window.location.href = 'items/snare1.html';
 }
+
+let addButton = document.getElementById('add-button');
+addButton.addEventListener('click', addToCart);
+
+function addToCart(){
+        alert('Click the restart button to try again');
+};
