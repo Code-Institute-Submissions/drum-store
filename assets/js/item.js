9970b3d74affe1110d8bf670908e877c914c7222
Jonathanrange <jonathan.range@outlook.com>
2022-01-13 17:39:07 +0000

    Add links and information to specific items

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,5 +1,29 @@
 let addButton = document.getElementById('add-button');
+let removeButton = document.getElementById('remove-button');
+let itemPrice = document.getElementById('price');
+let cart = [];
+let itemCounter = -1;
+
 addButton.addEventListener('click', addToCart);
 function addToCart(){
-       addButton.innerHTML = 'Added to cart!'; 
+    itemCounter ++;
+    addButton.innerHTML = 'Added to cart!';
+    cart.push(itemPrice.innerHTML);
+
+    console.log(cart);
+    console.log(itemCounter);
 };
+
+removeButton.addEventListener('click', removeFromCart);
+function removeFromCart(){
+    if (itemCounter <= -1){
+        alert('Your cart is empty');
+    }
+    else {
+    itemCounter --;
+    cart.pop([itemCounter]);
+    };
+
+    console.log(cart);
+    console.log(itemCounter);
+}
--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-let snareOne = document.getElementById('snare1');
-
-snareOne.addEventListener('click', popUp);
-function popUp(){
-    window.location.href = 'items/snare1.html';
-}
